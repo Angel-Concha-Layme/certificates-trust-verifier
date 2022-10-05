a4bb9313cb3f8d5602799d923c57c1e4f54cd260
Sharon CR <schullunquia@unsa.edu.pe>
2022-10-05 09:05:17 -0500

    add error messages and documentation comments

--- verifierApp/verifierApp/settings.py
+++ verifierApp/verifierApp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from django.contrib.messages import constants as messages
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -132,3 +133,11 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-dark',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
--- verifierApp/verifierApp/src/verify.py
+++ verifierApp/verifierApp/src/verify.py
@@ -1,8 +1,39 @@
+import requests
 import random
 
 def get_results(url):
+  '''
+  Función que analiza y permite visualizar el nivel de confianza del
+  certificado digital de la URL ingresada
+  '''
   print(url)
   result = [random.sample(['green', 'white' ,'white'],3),
             random.sample(['white', 'white' ,'green'],3),
             random.sample(['white', 'green' ,'white'],3)]
   return result
+
+def is_valid_URL(url):
+  '''
+  Función que valida la sintaxis y existencia de una URL en Internet
+  '''
+  is_valid = True
+  response = ""
+  try:
+    response = requests.get(url, timeout = 3) # 3 segundos
+    print("URL is valid and exists on the internet")
+  # Si la URL supera el tiempo de espera (3 segundos)
+  except requests.exceptions.Timeout:
+    response = "Timeout error"
+    print(response)
+    is_valid = False
+  # Si la URL no existe en Internet
+  except requests.ConnectionError:
+    response = "URL does not exist on Internet or invalid syntax"
+    print(response)
+    is_valid = False
+  # Si la URL no tiene la implementación del protocolo HTTPS
+  except requests.exceptions.RequestException:
+    response = "Invalid syntax"
+    print(response)
+    is_valid = False
+  return is_valid, response
