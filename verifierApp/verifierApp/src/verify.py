0d19fc751e51fd25c1a464fcd1a557a614e781bf
Sharon CR <schullunquia@unsa.edu.pe>
2022-10-05 06:27:18 -0500

    remove a column of circles

--- verifierApp/verifierApp/src/verify.py
+++ verifierApp/verifierApp/src/verify.py
@@ -2,7 +2,7 @@ import random
 
 def get_results(url):
   print(url)
-  result = [random.sample(['red', 'white' ,'white', 'white'],4),
-            random.sample(['white', 'white' ,'green', 'white'],4),
-            random.sample(['white', 'green' ,'white', 'white'],4)]
+  result = [random.sample(['green', 'white' ,'white'],3),
+            random.sample(['white', 'white' ,'green'],3),
+            random.sample(['white', 'green' ,'white'],3)]
   return result
