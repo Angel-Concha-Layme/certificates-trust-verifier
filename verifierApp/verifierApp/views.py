85c620d1dea39220da7ce1f428cdac409b696ca4
Sharon CR <schullunquia@unsa.edu.pe>
2022-10-16 00:48:02 -0500

    add messages to display to the user

--- verifierApp/verifierApp/views.py
+++ verifierApp/verifierApp/views.py
@@ -8,13 +8,20 @@ from .src.verify import get_results, is_valid_URL, get_file_valid_urls
 lista_urls = []
 lista_colors = []
 lista_browsers = ['Microsoft Edge', 'Google Chrome', 'Mozilla Firefox']
-display = True
-
+display_button = True
+display_warning = False
+display_error = False
+display_success = False
+message_response = ""
 
 def index(request):
   global lista_colors
   global lista_urls
-  display = True
+  global display_warning
+  global display_error
+  global display_success
+  global message_response
+  display_button = True
   if request.method == 'POST':
     form = urlForm(request.POST)
     if form.is_valid():
@@ -38,7 +45,7 @@ def index(request):
         context = {'form': form,
                     'lista_browsers':lista_browsers,
                     'results':results,
-                    'display': display}
+                    'display': display_button}
       # si no es válida y no existe la URL
       else:
         # Para mostrar mensajes de error
@@ -46,39 +53,54 @@ def index(request):
 
         # si la lista de URLs esta vacia
         if len(lista_urls) == 0:
-          display = False
+          display_button = False
           context = {'form': form,
-                      'display': display}
+                      'display': display_button}
         # si la lista de URLs no esta vacia
         else:
           results = zip(lista_urls, lista_colors)
           context = {'form': form,
                       'lista_browsers':lista_browsers,
                       'results':results,
-                      'display': display}
+                      'display': display_button}
       return render(request, 'form.html', context)
 
   elif request.method == 'GET':
+    # mensajes de error, warning o éxito en el procesamiento del archivo
+    if display_warning == True:
+      messages.add_message(request, messages.WARNING, message_response)
+      display_warning = False
+    elif display_error == True:
+      messages.add_message(request, messages.ERROR, message_response)
+      display_error = False
+    elif display_success == True:
+      messages.add_message(request, messages.SUCCESS, message_response)
+      display_success = False
+
     form = urlForm()
     results = zip(lista_urls, lista_colors)
 
     # si la lista de URLs esta vacía
     if len(lista_urls) == 0:
-      display = False
-      context = {'form': form, 'display': display}
+      display_button = False
+      context = {'form': form, 'display': display_button}
     # si la lista de URLs NO esta vacía
     else:
-      display = True
+      display_button = True
       context = {'form': form,
                 'lista_browsers':lista_browsers,
                 'results':results,
-                'display': display}
+                'display': display_button}
     return render(request, 'form.html', context)
 
 def upload_file(request):
   global lista_colors
   global lista_urls
-  global display
+  global display_button
+  global display_warning
+  global display_error
+  global display_success
+  global message_response
   if request.method == 'POST':
 
     # leemos el archivo y lo obtenemos en bytes
@@ -94,8 +116,19 @@ def upload_file(request):
     lista_urls = urls + lista_urls
     lista_colors = colors + lista_colors
 
-    if len(lista_urls) > 0:
-      display = True
+    # verificamos si todas las URLs del archivo han sido procesadas
+    # caso contrario mostramos mensajes de error al usuario
+    if len(urls) > 0:
+      display_button = True
+      if len(file_urls) != len(urls) :
+        display_warning = True
+        message_response = "Existen URLs no válidas que no han sido procesadas"
+      else:
+        display_success = True
+        message_response = "Todas las URLs han sido procesadas exitosamente"
+    else:
+      display_error = True
+      message_response = "Todas las URLs son inválidas"
 
   return redirect('index')
 
