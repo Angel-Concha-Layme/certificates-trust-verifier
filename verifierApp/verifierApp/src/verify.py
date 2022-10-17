8c015bceee0f44fe8ca9c8d0290c361b3f7c8956
Angel Concha Layme <aconchal@unsa.edu.pe>
2022-10-17 00:56:50 -0500

    Update verify.py

--- verifierApp/verifierApp/src/verify.py
+++ verifierApp/verifierApp/src/verify.py
@@ -3,6 +3,11 @@ import random
 from cryptography import x509
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
+from OpenSSL import SSL, crypto
+from datetime import datetime
+import OpenSSL
+import socket
+
 
 def get_results(url):
   '''
@@ -188,4 +193,64 @@ def get_trust_stores():
   mozilla_firefox = structure_trust_store(mozilla_firefox)
 
   return microsoft_edge, google_chrome, mozilla_firefox
+
+def getPEMFile(url, port):
+  dst = (url, port)
+  ctx = SSL.Context(SSL.SSLv23_METHOD)
+  s = socket.create_connection(dst)
+  s = SSL.Connection(ctx, s)
+  s.set_connect_state()
+  s.set_tlsext_host_name(str.encode(dst[0]))
+
+  s.sendall(str.encode('HEAD / HTTP/1.0\n\n'))
+
+  peerCertChain = s.get_peer_cert_chain()
+  pemFile = ''
+
+  for cert in peerCertChain:
+      pemFile += crypto.dump_certificate(crypto.FILETYPE_PEM, cert).decode("utf-8")
+
+  return pemFile, peerCertChain
+
+def get_x509(cert): # un solo PEM
+    x509 = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, cert)# lee una cadena pem
+    result = {
+        'subject': dict(x509.get_subject().get_components()),
+        'issuer': dict(x509.get_issuer().get_components()),
+        'serialNumber': x509.get_serial_number(),
+        'version': x509.get_version(),
+        'notBefore': datetime.strptime(x509.get_notBefore().decode('ascii'), '%Y%m%d%H%M%SZ'),
+        'notAfter': datetime.strptime(x509.get_notAfter().decode('ascii'), '%Y%m%d%H%M%SZ'),
+    }
+    extensions = (x509.get_extension(i) for i in range(x509.get_extension_count()))
+    extension_data = {e.get_short_name().decode('UTF-8'): str(e) for e in extensions}
+    result.update(extension_data)
+    return result
+
+def print_certchain_props(certchain):
+    for pos, cert in enumerate(certchain):
+        print("Certificate #" + str(pos))
+        for component in cert.get_subject().get_components():
+            print("Subject %s: %s" % (component))
+        datefrom = datetime.strptime(cert.get_notBefore().decode('ascii'), '%Y%m%d%H%M%SZ')
+        dateto = datetime.strptime(cert.get_notAfter().decode('ascii'), '%Y%m%d%H%M%SZ')
+        print("notBefore:" + datefrom.strftime("%d/%m/%Y"))
+        print("notAfter:" + dateto.strftime("%d/%m/%Y"))
+        print("version:" + str(cert.get_version()))
+        print("sigAlg:" + cert.get_signature_algorithm().decode('utf-8'))
+        print("digest:" + cert.digest('sha256').decode('utf-8'))
+        print("sha1:" + cert.digest('sha1').decode('utf-8'))
+        print("serial number:" + str(cert.get_serial_number()))
+
+def get_root_cert(certchain):
+    return certchain[2]
+
+def get_serial_number(cert):
+    return str(cert.get_serial_number())
+
+def get_sha256(cert):
+    return cert.digest('sha256').decode('utf-8')
+
+def get_sha1(cert):
+    return cert.digest('sha1').decode('utf-8')
 
