ba20c2ad8a3e6292539df03dd66786bdc8e2ae83
Angel Concha Layme <aconchal@unsa.edu.pe>
2022-10-17 16:42:28 -0500

    Update verify.py

--- verifierApp/verifierApp/src/verify.py
+++ verifierApp/verifierApp/src/verify.py
@@ -395,10 +395,3 @@ FIN DE CADENA DE CERTIFICACION
 
 
 
-
-def validate_url(url):
-    url_pattern = "^https:\\/\\/(?:www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)$"
-    if re.match(url_pattern, url):
-        return True
-    else:
-        return False
