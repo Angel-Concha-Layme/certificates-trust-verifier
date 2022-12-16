1b17f8ec626623ca0d13ab89629e0e970e750aa9
Angel Concha Layme <aconchal@unsa.edu.pe>
2022-12-15 22:47:50 -0500

    Update certificate.py

--- verifierApp/verifierApp/src/certificate.py
+++ verifierApp/verifierApp/src/certificate.py
@@ -1,6 +1,6 @@
 from oscrypto import tls
 from certvalidator import CertificateValidator, errors
-from .verify import get_trust_stores
+from verify import get_trust_stores
 import re
 
 
@@ -35,12 +35,15 @@ def get_certificate_chain(url):
 def generate_dict_chain(chain):
     """
     Funcion que genera un arreglo de diccionario con los certificados de la cadena de certificados
+    Retorna un arreglo de diccionarios vacio si la cadena de certificados es None (no tiene certificados)
     """
     dict_chain =[]
+    if chain is None:
+        return dict_chain
     for cert in chain:
         dict_cert = {
             "Subject" : cert.subject.native,
-            "Issuer" : cert.issuer.native,
+            "Isuuer" : cert.issuer.native,
             "Serial Number" : hex(cert.serial_number).upper(),
             "Not Valid Before" : cert.not_valid_before,
             "Not Valid After" : cert.not_valid_after,
@@ -52,11 +55,9 @@ def generate_dict_chain(chain):
         dict_chain.append(dict_cert)
     return dict_chain
 
-
 microsft_edge, google_chrome, mozilla_firefox = get_trust_stores()
 
-
-def validate_chain(dict_chain, trust_store):
+def security_level(dict_chain, trust_store):
     """
     Funcion que valida que el sha1 raiz de la cadena de certificados este en el trust store (trust_store es un arreglo de diccionarios)
     Funcion que valida si la fecha de expiracion del certificado raiz es mayor a la fecha actual: 
@@ -66,6 +67,11 @@ def validate_chain(dict_chain, trust_store):
     is_sha1_in_trust_store = False
     is_valid_date = False
 
+    if dict_chain == []:
+        security_level = 1
+        return security_level
+
+    size_chain = len(dict_chain)
     for cert in trust_store:
         sha1 = ":".join(dict_chain[0]["SHA-1"][i:i+2] for i in range(0, len(dict_chain[0]["SHA-1"]), 3))
         if sha1 == cert["SHA-1"]:
@@ -73,12 +79,42 @@ def validate_chain(dict_chain, trust_store):
             validity_str = cert ["validity"] 
             validity = validity_str.split(" - ")
             if (dict_chain[0]["Not Valid After"].strftime("%Y-%m-%d")  ) >= (validity[0]):
-                print (dict_chain[0]["Not Valid After"].strftime("%Y-%m-%d"))
-                print (validity[0])
                 is_valid_date = True
 
     if (is_sha1_in_trust_store == True and is_valid_date == True):
         security_level = 3
 
+    # si el sha1 del certificado raiz esta en el trust store pero la fecha de expiracion es menor a la fecha actual entonces el nivel de seguridad es 2
+    if (is_sha1_in_trust_store == True and is_valid_date == False):
+        security_level = 2
+
+    # Si el certificado raiz es autofirmado entonces el nivel de seguridad es 1
+    if (dict_chain[0]["Subject"] == dict_chain[size_chain-1]["Isuuer"]):
+        security_level = 1
+
+    # Si el certificado raiz no esta en el trust store entonces el nivel de seguridad es 1
+    if (is_sha1_in_trust_store == False):
+        security_level = 1
+
+    # Si el certificado raiz es autofirmado y el sha1 del certificado raiz esta en el trust store entonces el nivel de seguridad es 2
+    if (dict_chain[0]["Subject"] == dict_chain[size_chain-1]["Isuuer"] and is_sha1_in_trust_store == True):
+        security_level = 2
+
     return security_level
 
+
+def view_security_level(url):
+    """
+    Funcion que retorna el nivel de seguridad de un sitio web a partir de su URL en formato https://www.ejemplo.com
+    """
+    chain = get_certificate_chain(url)
+    dict_chain = generate_dict_chain(chain)
+    Mozila = security_level(dict_chain, mozilla_firefox)
+    Chrome = security_level(dict_chain, google_chrome)
+    Edge = security_level(dict_chain, microsft_edge)
+
+    print("Mozilla Firefox: ", Mozila)
+    print("Google Chrome: ", Chrome)
+    print("Microsoft Edge: ", Edge)
+    
+    return Mozila, Chrome, Edge
