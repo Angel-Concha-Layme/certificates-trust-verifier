aeed93d560f57b67bae9d0b66e40388acb6447c5
Angel Concha Layme <aconchal@unsa.edu.pe>
2022-12-15 23:45:32 -0500

    Update verify.py

--- verifierApp/verifierApp/src/verify.py
+++ verifierApp/verifierApp/src/verify.py
@@ -185,13 +185,13 @@ def structure_trust_store(certificates):
     certificates_list.append(certificate_dic)
   return certificates_list
 
-def get_trust_stores():
+def get_trust_stores(): 
   '''
   Función retorna los trusts stores de los 3 navegadores
   '''
-  microsoft_edge = read_csv_certificates("verifierApp/verifierApp/static/data/Microsoft_Edge.csv")
-  google_chrome = read_pem_certificates("verifierApp/verifierApp/static/data/Google_Chrome.pem")
-  mozilla_firefox = read_pem_certificates("verifierApp/verifierApp/static/data/Mozilla_Firefox.pem")
+  microsoft_edge = read_csv_certificates("verifierApp/static/data/Microsoft_Edge.csv")
+  google_chrome = read_pem_certificates("verifierApp/static/data/Google_Chrome.pem")
+  mozilla_firefox = read_pem_certificates("verifierApp/static/data/Mozilla_Firefox.pem")
 
   microsoft_edge = structure_trust_store(microsoft_edge)
   google_chrome = structure_trust_store(google_chrome)
